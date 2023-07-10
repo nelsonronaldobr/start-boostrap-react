90f97fb2a1b8b84a15afce812acf278a135bd266
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-07-09 23:44:09 -0500

    configuring project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import  style from './styles-sheet/style.module.css'
+function App() {
+
+  return (
+    <>
+      <button className={style.button}  >Holi</button>
+    </>
+  )
+}
+
+export default App
