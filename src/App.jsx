4dd5a86feee8f12a6ef01ecc9e1b0a5a6d5f29de
NelsonBarreraRivas <nelsonbr862@gmail.com>
2023-07-11 18:28:17 -0500

    css modules and component props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
-import  style from './styles-sheet/style.module.css'
-function App() {
+//import style from './styles-sheet/style.module.css';
+
+import { Navbar } from './components/Navbar';
+
+export const App = () => {
+  const links = [
+    {
+      title: 'home',
+      href: '/home',
+    },
+    {
+      title: 'About',
+      href: '/about',
+    },
+  ];
+
+  const links2 = [
+    {
+      title: 'contact',
+      href: '/contact',
+    },
+    {
+      title: 'products',
+      href: '/products',
+    },
+  ];
 
   return (
-    <>
-      <button className={style.button}  >Holi</button>
-    </>
-  )
-}
-
-export default App
+    <div>
+      <Navbar
+        links={links}
+        manzana='1'
+        manzana2='2'>
+        {/* =========esto esta dentro========== */}
+        <p>hola</p>
+        <p>hola</p>
+      </Navbar>
+      <Navbar
+        links={links2}
+        manzana={2}
+        manzana2={['1', '2', '3']}
+        status={false}>
+        {/* =========esto esta dentro========== */}
+        <p>hola</p>
+        <p>hola</p>
+        <p>hola</p>
+        <p>hola</p>
+        <p>hola</p>
+        <p>hola</p>
+        <p>hola</p>
+        <p>hola</p>
+      </Navbar>
+    </div>
+  );
+};
