5e4ee211cb5be64bb97b6a70110d114f7daa8d33
NelsonBarreraRivas <nelsonbr862@gmail.com>
2023-07-22 20:57:37 -0500

    cambio para que aumente su puntaje del review

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ export const CardContainer = ({products, className}) => {
     return (
         <div className={className}> 
             {products.map((product,index) => (
-             <ProductCard key={product.id} producto={product}>{product.description}</ProductCard>
+             <ProductCard key={product.id} product={product}>{product.description}</ProductCard>
             )
 
            ) }
@@ -14,8 +14,7 @@ export const CardContainer = ({products, className}) => {
     )
 }
 
-const ProductCard= ( {children,producto})=>{
-    const {name,price} = producto;
+const ProductCard= ( {children, product : { name, price }})=>{
 return(
     <div>
     <h1>{name}</h1>
