0f4a04a65b9635407530f5801c7780811573cdc7
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-07-26 23:18:47 -0500

    add images

--- src/components/sections/SectionPortfolio.jsx
+++ src/components/sections/SectionPortfolio.jsx
@@ -1,7 +1,15 @@
 import { Title } from '../Title';
 import { Container } from '../Container';
 import { IconStart } from '../IconStarts';
+
+import cabin from '../../assets/images/cabin.png';
+import cake from '../../assets/images/cake.png';
+import circus from '../../assets/images/circus.png';
+import game from '../../assets/images/game.png';
+import safe from '../../assets/images/safe.png';
+import submarine from '../../assets/images/submarine.png';
 import style from '../../css-modules/SectionPortfolio.module.css';
+
 export const SectionPortfolio = ({ id }) => {
     return (
         <div id={id} className={style['section-portfolio__background']}> 
@@ -10,12 +18,12 @@ export const SectionPortfolio = ({ id }) => {
                     <Title className={style['section-contact__title']} >PORTFOLIO</Title>
                     <IconStart color={'black'} className={'start-icon__black'}/>
                     <div>
-                    <img/>
-                    <img/>
-                    <img/>
-                    <img/>
-                    <img/>
-                    <img/>
+                    <img src={cabin}/>
+                    <img src={cake}/>
+                    <img src={circus}/>
+                    <img src={game}/>
+                    <img src={safe}/>
+                    <img src={submarine}/>
                     </div>
                 </section>
             </Container>
