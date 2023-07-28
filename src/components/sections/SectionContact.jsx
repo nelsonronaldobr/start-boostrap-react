2e9865ec0093680808230ae532bead37eb1e3c57
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-07-27 22:02:46 -0500

    update changes in section form

--- src/components/sections/SectionContact.jsx
+++ src/components/sections/SectionContact.jsx
@@ -1,15 +1,43 @@
 import { Container } from '../Container';
 import { Title } from '../Title';
 import { IconStart } from '../IconStarts';
-import style from '../../css-modules/SectionContact.module.css'
+import style from '../../css-modules/SectionContact.module.css';
 export const SectionContact = ({ id }) => {
     return (
-        <div id={id} >
+        <div id={id}>
             <Container className={style['section-contact__background']}>
                 <section className={style['section-contact']}>
-                    <Title className={style['section-contact__title']}>CONTACT ME</Title>
-                    <IconStart className={'start-icon__black'} color={'black'}/>
-                </section> 
+                    <Title className={style['section-contact__title']}>
+                        CONTACT ME
+                    </Title>
+                    <IconStart
+                        className={'start-icon__black'}
+                        color={'black'}
+                    />
+                    <form className={`${style['section-contact__form']} ${style['container']}`}>
+                        <input
+                            type='text'
+                            className='input-field'
+                            placeholder='Name'
+                        />
+                        <input
+                            type='text'
+                            className='input-field'
+                            placeholder='Email Address'
+                        />
+                        <input
+                            type='text'
+                            className='input-field'
+                            placeholder='Phone Number'
+                        />
+                        <input
+                            type='text'
+                            className='input-field'
+                            placeholder='Message'
+                        />
+                        <button className='button-line'>Send</button>
+                    </form>
+                </section>
             </Container>
         </div>
     );
