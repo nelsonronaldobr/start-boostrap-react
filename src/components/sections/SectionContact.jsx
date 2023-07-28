5757016c250f4336312f4ad0a41aac0f3e3de59a
NelsonBarreraRivas <nelsonbr862@gmail.com>
2023-07-27 23:15:28 -0500

    section-contact arreglado

--- src/components/sections/SectionContact.jsx
+++ src/components/sections/SectionContact.jsx
@@ -4,41 +4,39 @@ import { IconStart } from '../IconStarts';
 import style from '../../css-modules/SectionContact.module.css';
 export const SectionContact = ({ id }) => {
     return (
-        <div id={id}>
-            <Container className={style['section-contact__background']}>
-                <section className={style['section-contact']}>
-                    <Title className={style['section-contact__title']}>
-                        CONTACT ME
-                    </Title>
-                    <IconStart
-                        className={'start-icon__black'}
-                        color={'black'}
-                    />
-                    <form className={`${style['section-contact__form']} ${style['container']}`}>
+        <Container className={style['section-contact__background']} id={id}>
+            <section className={style['section-contact']}>
+                <Title className={style['section-contact__title']}>
+                    CONTACT ME
+                </Title>
+                <IconStart className={'start-icon__black'} color={'black'} />
+                <div className={style.container}>
+                    <form className={style['section-contact__form']}>
                         <input
                             type='text'
-                            className='input-field'
+                            className={style['input-field']}
                             placeholder='Name'
                         />
                         <input
                             type='text'
-                            className='input-field'
+                            className={style['input-field']}
                             placeholder='Email Address'
                         />
                         <input
                             type='text'
-                            className='input-field'
+                            className={style['input-field']}
                             placeholder='Phone Number'
                         />
-                        <input
+                        <textarea
                             type='text'
-                            className='input-field'
+                            className={style['input-field']}
                             placeholder='Message'
-                        />
-                        <button className='button-line'>Send</button>
+                            rows='4'
+                            cols='50'></textarea>
+                        <button className={style['button-line']}>Send</button>
                     </form>
-                </section>
-            </Container>
-        </div>
+                </div>
+            </section>
+        </Container>
     );
 };
