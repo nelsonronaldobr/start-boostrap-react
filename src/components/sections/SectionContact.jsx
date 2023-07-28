7589704668550c24c791166e1365d4318b93da2b
NelsonBarreraRivas <nelsonbr862@gmail.com>
2023-07-28 00:55:21 -0500

    test 3

--- src/components/sections/SectionContact.jsx
+++ src/components/sections/SectionContact.jsx
@@ -4,39 +4,46 @@ import { IconStart } from '../IconStarts';
 import style from '../../css-modules/SectionContact.module.css';
 export const SectionContact = ({ id }) => {
     return (
-        <Container className={style['section-contact__background']} id={id}>
-            <section className={style['section-contact']}>
-                <Title className={style['section-contact__title']}>
-                    CONTACT ME
-                </Title>
-                <IconStart className={'start-icon__black'} color={'black'} />
-                <div className={style.container}>
-                    <form className={style['section-contact__form']}>
-                        <input
-                            type='text'
-                            className={style['input-field']}
-                            placeholder='Name'
-                        />
-                        <input
-                            type='text'
-                            className={style['input-field']}
-                            placeholder='Email Address'
-                        />
-                        <input
-                            type='text'
-                            className={style['input-field']}
-                            placeholder='Phone Number'
-                        />
-                        <textarea
-                            type='text'
-                            className={style['input-field']}
-                            placeholder='Message'
-                            rows='4'
-                            cols='50'></textarea>
-                        <button className={style['button-line']}>Send</button>
-                    </form>
-                </div>
-            </section>
-        </Container>
+        <div id={id}>
+            <Container className={style['section-contact__background']}>
+                <section className={style['section-contact']}>
+                    <Title className={style['section-contact__title']}>
+                        CONTACT ME
+                    </Title>
+                    <IconStart
+                        className={'start-icon__black'}
+                        color={'black'}
+                    />
+                    <div className={style.container}>
+                        <form className={style['section-contact__form']}>
+                            <input
+                                type='text'
+                                className={style['input-field']}
+                                placeholder='Name'
+                            />
+                            <input
+                                type='text'
+                                className={style['input-field']}
+                                placeholder='Email Address'
+                            />
+                            <input
+                                type='text'
+                                className={style['input-field']}
+                                placeholder='Phone Number'
+                            />
+                            <textarea
+                                type='text'
+                                className={style['input-field']}
+                                placeholder='Message'
+                                rows='4'
+                                cols='50'></textarea>
+                            <button className={style['button-line']}>
+                                Send
+                            </button>
+                        </form>
+                    </div>
+                </section>
+            </Container>
+        </div>
     );
 };
